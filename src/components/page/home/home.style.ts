03a910baac3e8694640103ceaa2ffeb46ca1cccd
jeffjd <jef.jdrio@gmail.com>
2022-03-24 02:33:05 -0300

    atualização de estilo no componente home

---
+++ src/components/page/home/home.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HomeLayout = styled.article`
+    background-color: var(--cultured);
+    color: var(--rich-black-frogra-39);
+    padding-left: 0.5vw;
+    padding-right: 0.5vw;
+    height: 70vh;
+    margin-top: 1vh;
+    margin-right: 1vw;
+    margin-left: 1vw;
+    p {
+        margin: 0px;
+    }
+`;
--- src/components/page/home/home.tsx
+++ src/components/page/home/home.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { HomeLayout } from "./home.style";
 
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <HomeLayout>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. SolutaF
+                reprehenderit quia fugiat explicabo odio exercitationem
+                assumenda modi minus harum debitis atque omnis nobis, veritatis
+                nisi eius aut nesciunt esse laudantium. Lorem ipsum dolor sit
+                amet consectetur, adipisicing elit. Ad, porro! Praesentium,
+                earum autem dignissimos eius delectus non minus repellendus
+                mollitia sequi ullam! Similique tempora cupiditate aspernatur
+                minus. Quas, molestias architecto! Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Veritatis explicabo fugiat ea
+                dicta optio? Id, inventore? Temporibus autem illum, similique
+                nihil id neque tempore corporis ipsum eaque expedita. Dolores,
+                minima.
+            </p>
+        </HomeLayout>
+    );
 };
 
 export default Home;
