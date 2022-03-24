b5e0baaf8b9f94e6db25cdd166ea30391df1e6b8
jeffjd <jef.jdrio@gmail.com>
2022-03-24 00:23:13 -0300

    criação de estilo no RootElement

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "./routes.style";
 
 import Header from "../header";
 import Footer from "../footer";
@@ -9,11 +10,15 @@ import Home from "../page/home";
 const RootElement = () => {
     return (
         <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-            <Footer />
+            <Container>
+                <Header />
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </Container>
         </BrowserRouter>
     );
 };
